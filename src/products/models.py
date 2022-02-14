1ef79e519cc160820cdf57373d166190469be36a
AlexandreHiroyuki <alex.hiroyuki@outlook.com>
2022-02-14 15:05:41 -0300

    added images on detail html

--- src/products/models.py
+++ src/products/models.py
@@ -14,11 +14,20 @@ def upload_image_path(instance, filename):
     final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
     return 'products/{new_filename}/{final_filename}'.format(new_filename=new_filename, final_filename=final_filename)
 
+class ProductManager(models.Manager):
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id=id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=0.00)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+
+    objects = ProductManager()
 
     def __str__(self):
         return self.title
--- src/products/views.py
+++ src/products/views.py
@@ -1,4 +1,4 @@
-from multiprocessing import context
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
 from .models import Product
@@ -11,8 +11,10 @@ def product_list_view(request):
     return render(request, 'products/list.html', context)
 
 def product_detail_view(request, pk=None, *args, **kwargs):
-    # instance = Product.objects.get(pk=pk)
-    instance = get_object_or_404(Product, pk=pk)
+    instance = Product.objects.get_by_id(pk)
+    if instance is None:
+        raise Http404('Product does not exists')
+    
     context = {
         'object': instance
     }
